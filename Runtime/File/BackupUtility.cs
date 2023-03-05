3fc4ff9600d9cfe619f826762f68273c8df84960
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2023-03-05 11:22:58 +0800

    把在Siglota搞的一些东西搬过来
    Signed-off-by: QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>

--- Runtime/File/BackupUtility.cs
+++ Runtime/File/BackupUtility.cs
@@ -2,9 +2,11 @@ namespace WilliamQiufeng.UnityUtils.File
 {
     public static class BackupUtility
     {
-        public static void BackupFor(string path, int maxBackupCount)
+        public delegate string GenerateFilePath(string path, int backupIndex);
+
+        public static void BackupFor(string path, int maxBackupCount, GenerateFilePath filePathGenerator = default)
         {
-            BackupFor(path, 0, maxBackupCount);
+            BackupFor(path, 0, maxBackupCount, filePathGenerator);
         }
 
         private static string BackupFilePath(string path, int backupIndex)
@@ -12,9 +14,10 @@ namespace WilliamQiufeng.UnityUtils.File
             return $"{path}.{backupIndex}.bak";
         }
 
-        private static void BackupFor(string path, int backupIndex, int backupCount)
+        private static void BackupFor(string path, int backupIndex, int backupCount,
+            GenerateFilePath filePathGenerator = default)
         {
-            var srcPath = backupIndex == 0 ? path : BackupFilePath(path, backupIndex);
+            var srcPath = backupIndex == 0 ? path : (filePathGenerator ?? BackupFilePath)(path, backupIndex);
             var backupPath = BackupFilePath(path, backupIndex + 1);
             if (System.IO.File.Exists(backupPath) && backupIndex + 1 < backupCount)
                 BackupFor(path, backupIndex + 1, backupCount);
